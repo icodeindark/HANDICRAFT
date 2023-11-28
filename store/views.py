652af07c80835d99a9dc5b9ace8972037fb058a7
icodeindark <abelmp999@gmail.com>
2023-11-28 10:12:15 +0530

    Initial commit

---
+++ store/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Category, Product
+
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+def home(request):
+    return render(request,'index.html')
+
+def all_products(request):
+    products = Product.products.all()
+    context = {'products': products}
+    return render(request, 'store/home.html', context)
+
+
+def category_list(request, category_slug=None):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    context= {'category': category, 'products': products}
+    return render(request, 'store/products/category.html', context )
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    context = {'product': product}
+    return render(request, 'store/products/detail.html', context )
+
+
+
