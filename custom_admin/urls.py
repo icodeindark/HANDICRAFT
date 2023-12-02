4c50a8afd3f5631025225e91c944054e126c07c1
icodeindark <abelmp999@gmail.com>
2023-12-02 15:08:55 +0530

    3rd commit

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('',views.admin_login,name='admin_login'),
     path('dashboard',views.admin_dashboard,name='admin_dashboard'),
+    path('product_management',views.product_management,name='product_management'),
 
 ]
--- custom_admin/views.py
+++ custom_admin/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def admin_login(request):
-    return render(request,'admin_templates/authentication_login.html')
+    return render(request,'admin_templates/authentication-login.html')
 
 def admin_dashboard(request):
     return render(request,'admin_templates/index.html')
+
+def product_management(request):
+    return render(request,'admin_templates/product_management.html')
