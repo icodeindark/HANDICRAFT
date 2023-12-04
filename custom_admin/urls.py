f5551c116fd6dfd020bd4f716d5300b0ce21c160
icodeindark <abelmp999@gmail.com>
2023-12-04 16:29:47 +0530

    updated

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('',views.admin_login,name='admin_login'),
     path('dashboard/',views.admin_dashboard,name='admin_dashboard'),
     path('product_management/', views.product_management, name='product_management'),
+    path('product_management/<slug:category_slug>/', views.product_admin, name='product_admin'),
+
+    path('update_product/<str:pk>/',views.updateProduct,name='update_product'),
 
 
 ]
--- custom_admin/views.py
+++ custom_admin/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 
 from store.models import Category,Product
 from .forms import ProductForm,CategoryForm
@@ -9,18 +9,30 @@ def admin_login(request):
     return render(request,'admin_templates/authentication-login.html')
 
 def admin_dashboard(request):
-    return render(request,'admin_templates/product_management.html')
+    return render(request,'admin_templates/index.html')
 
-'''
-def product_admin(request):
+
+def product_admin(request,category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
     context= {'category': category, 'products': products}
-    return render(request,'admin_templates/product_managment.html',context)
-'''
+    return render(request,'admin_templates/product_management.html',context)
 
 
 def product_management(request):
     products=Product.objects.all()
     context={'products':products}
     return render(request,'admin_templates/product_management.html',context)
+
+
+def updateProduct(request,pk):
+    product =Product.objects.get(id=pk)
+    form = ProductForm(instance=product)
+    if request.method == 'POST':
+        form = ProductForm(request.POST,instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        
+    context = {'form':form}
+    return render(request,'admin_templates/product_form.html',context)
